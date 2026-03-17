436ecfa0f6ab6792fcb463d542a387c71784e8e1
KwakuJO <kwakujotchere@gmail.com>
2026-03-17 04:57:50 -0600

    WhatAPain
    This update was hours of suffering
    - Turret placement works (NF)
    - Updated minor scriptings

--- Scripts/PlainTurret.gd
+++ Scripts/PlainTurret.gd
@@ -4,24 +4,27 @@ var turretHealth = 100.0
 var closestFire
 var timer = 0
 
+func _ready():
+	add_to_group("turrets")
+
 func _process(delta: float) -> void:
 	$ProgressBar.value = turretHealth
 	timer += delta
 	
 	if timer >= 1 && closestFire:
 		timer -= 1
-		print("SHOOT")
+		#print("SHOOT")
 		closestFire.fireHealth -= 50
 		if closestFire.fireHealth <= 0:
 			closestFire = null
 	elif timer >= 2:
-		print('TIMER')
+		#print('TIMER')
 		timer -= 1
 	
 	if turretHealth <= 0:
-		print('KIA')
+		#print('KIA')
 		queue_free()
 
 func _on_fire_enter_area(body: Node2D) -> void:
 	closestFire = body
-	print('FIRE DETECTED')
+	#print('FIRE DETECTED')
